2bef6373f25beb59816aa15cb01e20370e2b407a
VictorRosaValdez <victorrosavaldez@hotmail.com>
2023-02-19 19:13:43 +0100

    DbContext added

--- BioscoopSysteemAPI/BioscoopSysteemAPI/Models/Room.cs
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Models/Room.cs
@@ -2,6 +2,7 @@
 {
     public class Room
     {
+        // Properties
         public int RoomId { get; set; }
         public bool InUse { get; set; }
         public int NumberOfSeatsAvailable { get; set; }
