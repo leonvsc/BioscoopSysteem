68abc3d1417bc94d37389974d7ac86f0d5f1bddb
VictorRosaValdez <victorrosavaldez@hotmail.com>
2023-02-19 18:27:51 +0100

    Models added.

---
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BioscoopSysteemAPI.Models
+{
+    public class Payment
+    {
+        // Properties
+        public int PaymentId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int Amount { get; set; }
+        
+        [MaxLength(50)]
+        public string PaymentMethod { get; set; }
+
+        // Navigation Property
+
+        public int ReservationId { get; set; }
+    }
+}
---
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Models/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace BioscoopSysteemAPI.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public bool InUse { get; set; }
+        public int NumberOfSeatsAvailable { get; set; }
+    }
+}
