90d1a5a3f52caf7ab0104e3017da9ae303b179e6
Huseyin Tufekci <h.tufekci@student.avans.nl>
2023-02-27 18:01:22 +0100

    BIOS-52-Controllers-Maken-ticket: Controllers added

---
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Controllers/PaymentController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BioscoopSysteemAPI.Models;
+using BioscoopSysteemAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BioscoopSysteemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly PaymentRepository paymentRepository;
+
+        public PaymentController(PaymentRepository paymentRepository)
+        {
+            this.paymentRepository = paymentRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Payment>>> GetPayments()
+        {
+            try
+            {
+                var payments = await this.paymentRepository.GetPayments();
+
+                if (payments == null)
+                {
+                    return NotFound();
+                }
+                return Ok(payments);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
+        }
+    }
+}
+
