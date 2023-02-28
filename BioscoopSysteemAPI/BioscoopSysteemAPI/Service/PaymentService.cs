4ed989f9dc4f13d0ee3167ba7df9334079e7a304
jesse <jessevannoort@gmail.com>
2023-02-28 16:24:58 +0100

    Added Movie and paymentservice

---
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Service/PaymentService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BioscoopSysteemAPI.Models;
+using BioscoopSysteemAPI.Repository;
+using System;
+
+public class PaymentService
+{
+	private readonly PaymentRepository paymentRepository;
+	public PaymentService(PaymentRepository paymentRepository)
+	{
+		this.paymentRepository = paymentRepository;
+	}
+
+	private IsEnumerable<Payment> GetPayments()
+    {
+		var payments = this.paymentRepository.GetPayments();
+		return payments;
+	}
+
+	private Payment GetPayment()
+    {
+		var payment = this.paymentRepository.GetPayment();
+		return payment;
+    }
+}
