e6cb96a308b07d7a9455079082d113b68ee3a188
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2023-03-02 11:48:39 +0100

    Fixed an issue with the movies file

--- BioscoopSysteemWeb/BioscoopSysteemWeb/Service/Movies.cs
+++ BioscoopSysteemWeb/BioscoopSysteemWeb/Service/Movies.cs
@@ -1,18 +1,18 @@
-﻿namespace BioscoopSysteemWeb.Service;
-
-public record Movies
-{
-    public int MovieId { get; set; }
-    
-    public string Name { get; set; }
-    
-    public string Description { get; set; }
-    
-    public int Price { get; set; }
-    
-    public byte AllowedAge { get; set; }
-    
-    public string? ImageUrl { get; set; }
-    
-    public int RoomId { get; set; }
-}
+﻿// namespace BioscoopSysteemWeb.Service;
+//
+// public record Movies
+// {
+//     public int MovieId { get; set; }
+//     
+//     public string Name { get; set; }
+//     
+//     public string Description { get; set; }
+//     
+//     public int Price { get; set; }
+//     
+//     public byte AllowedAge { get; set; }
+//     
+//     public string? ImageUrl { get; set; }
+//     
+//     public int RoomId { get; set; }
+// }
