250704402d75c2c9c211141dc45b6b1924330eaa
Boogieguy4 <89997608+Boogieguy4@users.noreply.github.com>
2023-03-04 22:12:26 +0100

    Seats uitgebreid.

---
+++ BioscoopSysteemAPI/BioscoopSysteemAPI/Profiles/SeatProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BioscoopSysteemAPI.DTOs.MovieDTOs;
+using BioscoopSysteemAPI.DTOs.SeatDTOs;
+using BioscoopSysteemAPI.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BioscoopSysteemAPI.Profiles
+{
+    public class SeatProfile : Profile
+    {
+        public SeatProfile()
+        {
+            // Mapping from the domain object to the readDTO object.
+            CreateMap<Seat, SeatReadDTO>();
+
+            // Mapping from the createDTO object to the domain object. ReverseMap is for navigating both ways.
+            CreateMap<SeatCreateDTO, Seat>().ReverseMap();
+
+            // Mapping from the createDTO object to the domain object. ReverseMap is for navigating both ways.
+            CreateMap<SeatUpdateDTO, Seat>().ReverseMap();
+
+            // Mapping from the domain object to the deleteDTO object.
+            CreateMap<Seat, SeatDeleteDTO>();
+
+        }
+    }
+}
