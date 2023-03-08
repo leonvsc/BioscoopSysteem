80e01fe77dc43822cd0ad20f4cbe6381de30939b
niels warnaar <nielswarnaar@nielss-MBP.dsc.local>
2023-03-08 12:36:26 +0100

    Fixed a typo

--- BioscoopSysteemWeb/BioscoopSysteemWeb/Service/MovieService.cs
+++ BioscoopSysteemWeb/BioscoopSysteemWeb/Service/MovieService.cs
@@ -20,19 +20,19 @@ namespace BioscoopSysteemWeb.Service
         {
             try
             {
-                var respose = await _httpClient.GetAsync("api/movies");
+                var response = await _httpClient.GetAsync("api/movies");
 
-                if (respose.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    if (respose.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
                     {
                         return Enumerable.Empty<MovieReadDTO>();
                     }
-                    return await respose.Content.ReadFromJsonAsync<IEnumerable<MovieReadDTO>>();
+                    return await response.Content.ReadFromJsonAsync<IEnumerable<MovieReadDTO>>();
                 }
                 else
                 {
-                    var message = await respose.Content.ReadAsStringAsync();
+                    var message = await response.Content.ReadAsStringAsync();
                     throw new Exception(message);
                 }
             }
@@ -46,20 +46,20 @@ namespace BioscoopSysteemWeb.Service
         {
             try
             {
-                var respose = await _httpClient.GetAsync($"api/movies/{id}");
+                var response = await _httpClient.GetAsync($"api/movies/{id}");
 
-                if (respose.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    if (respose.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
                     {
                         return default(MovieReadDTO);
                         
                     }
-                    return await respose.Content.ReadFromJsonAsync<MovieReadDTO>();
+                    return await response.Content.ReadFromJsonAsync<MovieReadDTO>();
                 }
                 else
                 {
-                    var message = await respose.Content.ReadAsStringAsync();
+                    var message = await response.Content.ReadAsStringAsync();
                     throw new Exception(message);
                 }
             }
